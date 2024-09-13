69de6e53fa83d7c1c4c94c376c27cbe8eb9da918
Oliver Cappis <Oliver_Cappis1@baylor.edu>
2024-09-13 14:19:55 -0500

    fixed get gerneal news api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+// require('dotenv').config();
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
